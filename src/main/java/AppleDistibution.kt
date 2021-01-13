9c40c6e8a421229aea28a5c5335d4d7becdc9430
Harshit Gupta <36876871+gharshit192@users.noreply.github.com>
2021-01-13 18:25:19 +0530

    Update AppleDistibution.kt

--- src/main/java/AppleDistibution.kt
+++ src/main/java/AppleDistibution.kt
@@ -1,3 +1,22 @@
+/*Problem 2 : My Money My Shares
+
+Ram , sham and rahim went for shopping apple. They bought apple worth 100 rupees. Ram paid 50 rupees, sham paid 30 rupees  and rahim paid 20 rupees. Each apple is tagged with its weight on it.  Write a program to distribute apples such that the quantity of apple they get is in best proportionate to the amount they paid. 
+
+Note : you cannot cut a apple into pieces have to allocate the whole apple to one of them.
+For example : 
+
+
+If there is 8 apple  of  400g , 100g  , 400g , 300g , 200g , 300g ,100g , 200g  then we can distribute them as 
+Ram :  400g , 400g , 200g  (total 1kg , 50%)
+Sham :  300g , 300g (tatal 600g , 30%)
+rahim , 200g , 100g , 100g  (tatal 400g , 20%)
+
+Distribution Result : 
+Ram : 400 400 100
+Sham : 300 , 300
+Rahim  : 200 , 100 , 100
+
+*/
 import java.util.*
 
 fun main() {

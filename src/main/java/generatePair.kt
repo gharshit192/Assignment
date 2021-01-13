e2f6123fea46f7d0f433a4d0c5bc67789a78f5a5
Harshit Gupta <36876871+gharshit192@users.noreply.github.com>
2021-01-13 18:26:48 +0530

    Update generatePair.kt

--- src/main/java/generatePair.kt
+++ src/main/java/generatePair.kt
@@ -1,3 +1,10 @@
+/*
+Write a program to find all pairs of elements in an array whose sum is equal to a specified number.
+            Example : {2, 7, 4, -5, 11, 5, 20}, 15 (Specified number enter by a user)
+
+            Output : (4,11) & (-5 ,20)
+*/
+
 import java.util.Arrays
 object generatePair {
     fun findThePairs(inputArray: IntArray, inputNumber: Int) {
